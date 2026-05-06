c5bce553d0df8ff95eccf5a03209303ce6c50256
kiyer789 <kiyer789@student.fuhsd.org>
2026-05-05 17:41:58 -0700

    hello

--- test_cs/core/src/main/java/io/github/tanishkolhe/csa_project/Main.java
+++ test_cs/core/src/main/java/io/github/tanishkolhe/csa_project/Main.java
@@ -1,32 +1,100 @@
 package io.github.tanishkolhe.csa_project;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
+public class Main implements ApplicationListener {
+    private SpriteBatch spriteBatch;
+    FitViewport viewport;
     private Texture image;
+    private Texture backgroundTexture;
+    private Texture bucketTexture;
+    private Texture dropTexture;
+    private Sound dropSound;
+    private Music music;
+    private Sprite bucketSprite;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        backgroundTexture = new Texture("background.png");
+        bucketTexture = new Texture("bucket.png");
+        dropTexture = new Texture("drop.png");
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(1,1);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        input();
+        logic();
+        draw();
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+
+        float worldWidth = viewport.getWorldWidth();
+
+        //the height of the viewport
+        float worldHeight = viewport.getWorldHeight();
+        spriteBatch.draw(backgroundTexture, 0, 0,worldWidth,worldWidth);
+        bucketSprite.draw(spriteBatch);
+        
+        
+        
+
+        spriteBatch.end();
+    }
+
+    private void logic() {
+        
+    }
+
+    private void input() {
+        float speed = 1f;
+        float delta = Gdx.graphics.getDeltaTime();
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            bucketSprite.translateX(speed*delta);
+        }
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        
+    }
+
+    @Override
+    public void pause(){
+
+    }
+
+    @Override
+    public void resume(){
+
+    }
+
+    @Override
+    public void resize(int width,int height){
+        viewport.update(width, height,true);
     }
 }
