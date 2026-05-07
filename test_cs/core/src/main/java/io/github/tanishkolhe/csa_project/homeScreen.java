c80055aeea7f25d00075542aa3c2e8be7f2a4f64
TanishKolhe <tanish.kolhe@gmail.com>
2026-05-07 13:08:31 -0700

    g

--- test_cs/core/src/main/java/io/github/tanishkolhe/csa_project/homeScreen.java
+++ test_cs/core/src/main/java/io/github/tanishkolhe/csa_project/homeScreen.java
@@ -1,7 +1,9 @@
 package io.github.tanishkolhe.csa_project;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,6 +14,8 @@ public class homeScreen implements Screen{
     private final Main game;
     
     private Texture background;
+    private Texture ghost;
+    private Sprite ghost2;
     TextButton button;
     
 
@@ -32,7 +36,9 @@ public class homeScreen implements Screen{
     public void show(){
         //button = new TextButton("Click Me!", skin);
         background = new Texture("csclassroom.jpg");
-        
+        ghost = new Texture("Ghost.png");
+        ghost2 = new Sprite(ghost);
+
     }
 
 
@@ -48,10 +54,13 @@ public class homeScreen implements Screen{
         game.batch.setColor(0.4f, 0.4f, 0.4f, 1f);
         
         game.batch.begin();
+        ghost2.setPosition(2.5f,1.1f);
+        ghost2.setSize(1f, 1.11f);
 
         game.batch.setColor(0.4f, 0.4f, 0.4f, 1f);
         game.batch.draw(background, 0, 0,game.myViewport.getWorldWidth(),game.myViewport.getWorldHeight());
         game.batch.setColor(1f, 1f, 1f, 1f);
+        ghost2.draw(game.batch);
 
         game.batch.end();
     }
