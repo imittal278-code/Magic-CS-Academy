6e3dffbf14a42dbea57e3cd8d7e6cff2d0fb7d44
imittal278-code <imittal278@student.fuhsd.org>
2026-05-08 19:00:04 -0700

    Basic version of game
    *Ghosts can move linearly
    *Added Momo the cat :DD
    *Users can type numbers *in the libgdx window* to remove ghosts

--- Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/homeScreen.java
+++ Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/homeScreen.java
@@ -2,21 +2,34 @@ package io.github.magiccsacademy.csa_project;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import java.util.*;
 
 public class homeScreen implements Screen{
     private final Main game;
-    
+
+    private float catx = 0.0f, caty = 1.0f;
+    private ArrayList<Float> ghostx, ghosty;
     private Texture background;
     private Texture ghost;
     private Sprite ghost2;
+    private Texture cat;
+    private Sprite cat2;
+    private float ghostSpeed = 0.5f; // the ghostSpeed should (1) not be constant b/c well have slower bosses, (2) should be time dependent instead.
+    private BitmapFont font;
     TextButton button;
+    private Ghostturn turn;
     
 
 
@@ -38,29 +51,63 @@ public class homeScreen implements Screen{
         background = new Texture("csclassroom.jpg");
         ghost = new Texture("Ghost.png");
         ghost2 = new Sprite(ghost);
-
+        cat = new Texture("Momo2023.png");
+        cat2 = new Sprite(cat);
+        font = new BitmapFont();
+        font.getData().setScale(0.02f);
+        font.setUseIntegerPositions(false);
+        font.setColor(Color.YELLOW);
+        turn = new Ghostturn(4, 3, 1, true);
+        ghostx = new ArrayList<Float> ();
+        ghosty = new ArrayList<Float>();
+        for(int i=0;i<turn.ghostspresent.size();i++){
+            ghosty.add(((float)i)*0.5f);
+            ghostx.add(1.0f+i);
+        }
     }
 
 
 
      @Override
     public void render(float delta) {
+
         ScreenUtils.clear(0, 0, 0, 1);
         game.myViewport.apply();
         game.batch.setProjectionMatrix(game.myViewport.getCamera().combined);
 
-        
-
         game.batch.setColor(0.4f, 0.4f, 0.4f, 1f);
         
         game.batch.begin();
-        ghost2.setPosition(2.5f,1.1f);
-        ghost2.setSize(1f, 1.11f);
+        for(int i=0;i<10;i++){
+            if(Gdx.input.isKeyJustPressed(Input.Keys.NUM_0+i))turn.shapeDrawn(i);
+        }
+
+        cat2.setPosition(catx, caty);
+        cat2.setSize(1f, 1.11f);
 
         game.batch.setColor(0.4f, 0.4f, 0.4f, 1f);
         game.batch.draw(background, 0, 0,game.myViewport.getWorldWidth(),game.myViewport.getWorldHeight());
         game.batch.setColor(1f, 1f, 1f, 1f);
-        ghost2.draw(game.batch);
+
+        for(int i=0;i<turn.ghostspresent.size();i++){
+            Ghost g=turn.ghostspresent.get(i);
+            if(g.isAlive()){
+                float dx = catx - ghostx.get(i);
+                float dy = caty - ghosty.get(i);
+
+                float distance = (float) Math.sqrt(dx*dx+dy*dy);
+                if(distance>0.01f){
+                    ghostx.set(i,ghostx.get(i)+(dx/distance)*ghostSpeed*delta); // delta is amt o/ time since last frame
+                    ghosty.set(i,ghosty.get(i)+(dy/distance)*ghostSpeed*delta);
+                }
+                ghost2.setPosition(ghostx.get(i),ghosty.get(i));
+                ghost2.setSize(1f,1.11f);
+                font.draw(game.batch, turn.ghostspresent.get(i).shapes.toString(), ghostx.get(i) + 0.15f, ghosty.get(i) + 1.25f);
+                ghost2.draw(game.batch);
+            }
+        }
+
+        cat2.draw(game.batch);
 
         game.batch.end();
     }
