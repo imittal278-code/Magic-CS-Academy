e90f1b67d61c527d67504c1f6cf4ac71c6a19bf6
imittal278-code <imittal278@student.fuhsd.org>
2026-05-14 12:12:28 -0700

    Create Cat.java

---
+++ Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/Cat.java
@@ -0,0 +1,17 @@
+package io.github.magiccsacademy.csa_project;
+
+public class Cat {
+    
+    float getX(){
+        return 0f;
+    }
+    float getY(){
+        return 0f;
+    }
+    boolean loseLife(){
+        return true;
+    }
+    boolean isAlive(){
+        return true;
+    }
+}
