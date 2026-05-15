776dedb6cc238ea702fcdab1af16f7b74235be0f
TanishKolhe <tanish.kolhe@gmail.com>
2026-05-14 17:26:38 -0700

    done cat class
    🐱

--- Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/Cat.java
+++ Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/Cat.java
@@ -1,17 +1,65 @@
 package io.github.magiccsacademy.csa_project;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import java.util.*;
+
 public class Cat {
-    
-    float getX(){
-        return 0f;
+    float catx, caty;
+    int lives;
+    Texture cat;
+    Sprite cat2;
+    boolean alive;
+
+    public Cat() {
+        catx = 2.5f;
+        caty = 0.9765f;
+        lives = 3;
+        alive = true;
+        cat = new Texture("Momo2023.png");
+        cat2 = new Sprite(cat);
+        cat2.setSize(1f, 1.047f);
     }
-    float getY(){
-        return 0f;
+    void draw(SpriteBatch batch) {
+        cat2.setPosition(catx, caty);
+        cat2.draw(batch);
     }
-    boolean loseLife(){
-        return true;
+    void loseLife() {
+        lives--;
+        if (lives<=0) {
+            alive = false;
+        }
     }
-    boolean isAlive(){
-        return true;
+    void gainLife() {
+        lives++;
     }
+    boolean isAlive() {
+        return alive;
+    }
+    float getX() {
+        return catx;
+    }
+    float getY() {
+        return caty;
+    }
+    int getLives() {
+        return lives;
+    }
+    void setPosition(float x, float y) {
+        catx = x;
+        caty = y;
+    }
+
 }
