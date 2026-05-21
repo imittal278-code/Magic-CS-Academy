c51a73d05dfa2f637ae475ec2470440910a26193
imittal278-code <imittal278@student.fuhsd.org>
2026-05-21 13:03:34 -0700

    New constructors for Ghostturnn

--- Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/GameThing.java
+++ Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/GameThing.java
@@ -9,14 +9,13 @@ public class GameThing {
     public GameThing(){
        /* l3.addTurn(new Ghostturn(3,2,1,false));
         l3.addTurn(new Ghostturn(3,2,1,false));
-        l3.addTurn(new Ghostturn(2,6,1,false));
-        l3.addTurn(new Ghostturn(4,1,1,false));
+        l3.addTurn(new Ghostturn(2,6,1,false));*/
+        l3.addTurn(new Ghostturn(4,1,1,false, true));
         l3.addTurn(new Ghostturn(15,1,1,false));
         l3.addTurn(new Ghostturn(1,1,1,false));
         l3.addTurn(new Ghostturn(1,1,1,false));
-        l3.addTurn(new Ghostturn(1,1,1,false));*/
-        l3.addTurn(new Ghostturn(new Ghost("4")));
-        l3.addTurn(new Ghostturn(new Ghost("1111111111"))); 
+        l3.addTurn(new Ghostturn(1,1,1,false));
+        l3.addTurn(new Ghostturn(new Ghost("1111111111")));
         l3.addTurn(new Ghostturn(1,1,1,false));
         l3.addTurn(new Ghostturn(1,1,1,false));
         l3.addTurn(new Ghostturn(new Ghost("0000000000"))); 
