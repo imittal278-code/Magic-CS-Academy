12db96bcda584a930659d6b5324f94f5f873787e
imittal278-code <imittal278@student.fuhsd.org>
2026-05-21 13:22:02 -0700

    Yes

--- Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/GameThing.java
+++ Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/GameThing.java
@@ -7,9 +7,9 @@ public class GameThing {
     public Level l3 = new Level(1,1);
 
     public GameThing(){
-       /* l3.addTurn(new Ghostturn(3,2,1,false));
         l3.addTurn(new Ghostturn(3,2,1,false));
-        l3.addTurn(new Ghostturn(2,6,1,false));*/
+        l3.addTurn(new Ghostturn(3,2,1,false));
+        l3.addTurn(new Ghostturn(2,6,1,false));
         l3.addTurn(new Ghostturn(4,1,1,false, true));
         l3.addTurn(new Ghostturn(15,1,1,false));
         l3.addTurn(new Ghostturn(1,1,1,false));
