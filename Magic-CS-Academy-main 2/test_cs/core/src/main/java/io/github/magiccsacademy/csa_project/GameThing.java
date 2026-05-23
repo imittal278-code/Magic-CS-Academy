bafedb2e5107dee06aa7130f9207b1563e5c64ad
imittal278-code <imittal278@student.fuhsd.org>
2026-05-23 14:25:07 -0700

    Fixed the level with 15 ghosts

--- Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/GameThing.java
+++ Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/GameThing.java
@@ -12,7 +12,7 @@ public class GameThing {
         l4 = new Level(4, 1,game);
         l1 = new Level(1,1,game);
         l2 = new Level(2,2,game);
-        l3.addTurn(new Ghostturn(3, 2, 1, false,false,false));
+      l3.addTurn(new Ghostturn(3, 2, 1, false,false,false));
         l3.addTurn(new Ghostturn(3, 2, 1, false,false,false));
         l3.addTurn(new Ghostturn(2, 6, 1, false,false,false));
         l3.addTurn(new Ghostturn(4, 1, 1, false, true,false));
@@ -32,9 +32,7 @@ public class GameThing {
         l4.addTurn(new Ghostturn(20, 1, 1, false,false,false));// lightning
         l4.addTurn(new Ghostturn(new Ghost("121212"))); // Lightbulb
         l4.addTurn(new Ghostturn(new Ghost("13131313")));// anglerfish boss
-        //l4.addTurn(new Ghostturn(new Ghost("12321")));// lightbulb
         l4.addTurn(new Ghostturn(1,5,1,false,false,false));
-        //l4.addTurn(new Ghostturn(new Ghost("0112323110")));// anglerfish boss
         l4.addTurn(new Ghostturn(1, 10, 1, false, false, false));
         l4.addTurn(new Ghostturn(1, 5, 1, false,false,false));// lightbulb
         l4.addTurn(new Ghostturn(new Ghost("213020132")));// anglerfish boss last
@@ -47,12 +45,9 @@ public class GameThing {
         l1.addTurn(new Ghostturn(3,2,1,false,false,false));//SPIRAL make it true when spiral works
         l1.addTurn(new Ghostturn(4,4,1,false,true,false));//SHIELD remove comment when shield works
         l1.addTurn(new Ghostturn(8,1,1,false,false,false));//SPIRAL make it true when spiral works
-        //l1.addTurn(new Ghostturn(new Ghost("003003",true)));//when ready make the very last number(add it) the spiral number
         l1.addTurn(new Ghostturn(1, 6, 1, false, false, false));//when ready make the very last number(add it) the spiral number
-
         l1.addTurn(new Ghostturn(new Ghost("212121",true)));//when ready make the very last number(add it) the spiral number
-        //l1.addTurn(new Ghostturn(new Ghost("01213",true)));//when ready make the very last number(add it) the spiral number
-        l1.addTurn(new Ghostturn(1, 5, 1, false, false, false));//when ready make the very last number(add it) the spiral number
+        l1.addTurn(new Ghostturn(1, 5, 1, false, false, false));//when ready make the very last number(add it) the spiral number*/
 
 
         l2.addTurn(new Ghostturn(9,3,1,true,false,false));//Note this level is really hard, in the actual game fish come from the right and the cat is on the left
@@ -60,7 +55,7 @@ public class GameThing {
         l2.addTurn(new Ghostturn(7,3,1,true,true,false)); //SHIELD remove comment when shield worksNote this level is really hard, in the actual game fish come from the right and the cat is on the left
         l2.addTurn(new Ghostturn(1,8,1,true,false,false));
         l2.addTurn(new Ghostturn(4,3,1,true,false,false));
-        l2.addTurn(new Ghostturn(1,5,1,true,false,false));//add lightning if we get there
+        l2.addTurn(new Ghostturn(1,5,1,true,false,false));//add lightning if we get there*/
 
 
         
