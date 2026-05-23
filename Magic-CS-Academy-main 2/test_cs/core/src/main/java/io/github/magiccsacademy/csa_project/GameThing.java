3368e0fd052a1b8a048ec9535fe1835f18ee362d
TanishKolhe <tanish.kolhe@gmail.com>
2026-05-23 15:50:00 -0700

    forgot to declare

--- Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/GameThing.java
+++ Magic-CS-Academy-main 2/test_cs/core/src/main/java/io/github/magiccsacademy/csa_project/GameThing.java
@@ -13,6 +13,7 @@ public class GameThing {
         l4 = new Level(4, 1,game);
         l1 = new Level(1,1,game);
         l2 = new Level(2,8,game);
+        l5 = new Level(5, 1,game);
       l3.addTurn(new Ghostturn(3, 2, 1, false,false,false));
         l3.addTurn(new Ghostturn(3, 2, 1, false,false,false));
         l3.addTurn(new Ghostturn(2, 6, 1, false,false,false));
